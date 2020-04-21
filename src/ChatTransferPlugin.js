3ae814c572edfd82a0d8d60aac3060b3c811d1fd
John Fischelli <john.fischelli@gmail.com>
2020-04-20 21:12:19 -0500

    improving the transfer chat function - no need for chat orchestration changes

--- src/ChatTransferPlugin.js
+++ src/ChatTransferPlugin.js
@@ -1,79 +1,73 @@
 import { FlexPlugin } from 'flex-plugin';
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 import React from 'react';
-import * as Flex from '@twilio/flex-ui'
-import ChatTransferButton from './ChatTransferButton';
+import * as Flex from '@twilio/flex-ui';
+import TransferButton from './components/TransferButton';
 
 const PLUGIN_NAME = 'ChatTransferPlugin';
+const DEFAULT_TRANSFER_MODE = 'COLD';
 
 export const setUpComponents = () => {
-  Flex.TaskCanvasHeader.Content.add(
-    <ChatTransferButton key="chat-transfer-button" />, {
-      if: props => props.channelDefinition.capabilities.has("Chat") && props.task.taskStatus === 'assigned'
-    }
-  );
-}
+	Flex.TaskCanvasHeader.Content.add(<TransferButton key="chat-transfer-button" />, {
+		sortOrder: 1,
+		if: (props) =>
+			props.channelDefinition.capabilities.has('Chat') && props.task.taskStatus === 'assigned',
+	});
+};
 
 export const setUpActions = () => {
-  Flex.Actions.replaceAction("TransferTask", (payload, original) => transferOverride(payload, original))
-}
-
-export const removeDefaultChatChannelOrchestrations = () => {
-  Flex.ChatOrchestrator.setOrchestrations("wrapup", []);
-  Flex.ChatOrchestrator.setOrchestrations("completed", ["LeaveChatChannel"]);
-}
-
-export const restoreDefaultChatChannelOrchestrations = () => {
-  Flex.ChatOrchestrator.setOrchestrations("wrapup", ["DeactivateChatChannel"]);
-  Flex.ChatOrchestrator.setOrchestrations("completed", ["DeactivateChatChannel", "LeaveChatChannel"]);
-}
+	Flex.Actions.replaceAction('TransferTask', (payload, original) =>
+		transferOverride(payload, original)
+	);
+};
 
 export const transferOverride = (payload, original) => {
-  if (!Flex.TaskHelper.isChatBasedTask(payload.task)) {
-    return original(payload);
-  }
+	if (!Flex.TaskHelper.isChatBasedTask(payload.task)) {
+		return original(payload);
+	}
 
-  return new Promise((resolve, reject) => {
-    removeDefaultChatChannelOrchestrations();
+	const mode = payload.options.mode || DEFAULT_TRANSFER_MODE;
 
-    const manager = Flex.Manager.getInstance();
+	return new Promise((resolve, reject) => {
+		const manager = Flex.Manager.getInstance();
+		const body = {
+			Token: manager.user.token,
+			mode: mode,
+			taskSid: payload.task.taskSid,
+			targetSid: payload.targetSid,
+			workerName: manager.user.identity,
+		};
 
-    fetch(`https://${manager.serviceConfiguration.runtime_domain}/transfer-chat`, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      method: 'POST',
-      body: `Token=${manager.user.token}&taskSid=${payload.task.taskSid}&destinationQueue=${payload.targetSid}&workerName=${manager.user.identity}`
-    })
-    .then(response => {
-      setTimeout(() => {
-        restoreDefaultChatChannelOrchestrations();
-      }, 2000)
-      resolve();
-    })
-    .catch(error => {
-      setTimeout(() => {
-        restoreDefaultChatChannelOrchestrations();
-      }, 2000)
-      reject();
-    });
-  })
-}
+		fetch(`https://${manager.serviceConfiguration.runtime_domain}/transfer-chat`, {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			method: 'POST',
+			body: JSON.stringify(body),
+		})
+			.then((response) => {
+				resolve();
+			})
+			.catch((error) => {
+				reject();
+			});
+	});
+};
 
 export default class ChatTransferPlugin extends FlexPlugin {
-  constructor() {
-    super(PLUGIN_NAME);
-  }
+	constructor() {
+		super(PLUGIN_NAME);
+	}
 
-  /**
-   * This code is run when your plugin is being started
-   * Use this to modify any UI components or attach to the actions framework
-   *
-   * @param flex { typeof import('@twilio/flex-ui') }
-   * @param manager { import('@twilio/flex-ui').Manager }
-   */
-  init(flex, manager) {
-    setUpComponents();
-    setUpActions();   
-  }
+	/**
+	 * This code is run when your plugin is being started
+	 * Use this to modify any UI components or attach to the actions framework
+	 *
+	 * @param flex { typeof import('@twilio/flex-ui') }
+	 * @param manager { import('@twilio/flex-ui').Manager }
+	 */
+	init(flex, manager) {
+		setUpComponents();
+		setUpActions();
+	}
 }
--- src/functions/transfer-chat.js
+++ src/functions/transfer-chat.js
@@ -1,65 +1,90 @@
 const JWEValidator = require('twilio-flex-token-validator').functionValidator;
 
-exports.handler = JWEValidator(async function(context, event, callback) {
+exports.handler = JWEValidator(async function (context, event, callback) {
+	// setup twilio client
+	const client = context.getTwilioClient();
 
-    // setup twilio client
-    const client = context.getTwilioClient();
+	// setup a response object
+	const response = new Twilio.Response();
+	response.appendHeader('Access-Control-Allow-Origin', '*');
+	response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS POST');
+	response.appendHeader('Content-Type', 'application/json');
+	response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    // parse data form the incoming http request
-    const originalTaskSid = event.taskSid;
-    const destinationQueueSid = event.destinationQueue;
-    const workerName = event.workerName;
+	// parse data form the incoming http request
+	const originalTaskSid = event.taskSid;
+	const targetSid = event.targetSid;
+	const workerName = event.workerName;
+	const mode = event.mode;
 
-    // setup a response object
-    const response = new Twilio.Response();
-    response.appendHeader('Access-Control-Allow-Origin', '*');
-    response.appendHeader('Access-Control-Allow-Methods', 'OPTIONS POST');
-    response.appendHeader('Content-Type', 'application/json');
-    response.appendHeader('Access-Control-Allow-Headers', 'Content-Type');
+	// retrieve original task's attributes
+	let originalTask = await client.taskrouter
+		.workspaces(context.TWILIO_WORKSPACE_SID)
+		.tasks(originalTaskSid)
+		.fetch();
+	let newAttributes = JSON.parse(originalTask.attributes);
 
-    // retrieve original task's attributes
-    let originalTask = await client.taskrouter.workspaces(context.TWILIO_WORKSPACE_SID).tasks(originalTaskSid).fetch();
-    let newAttributes = JSON.parse(originalTask.attributes);
+	// setup new task's attributes such that its linked to the
+	// original task in Flex Insights
+	if (!newAttributes.hasOwnProperty('conversations')) {
+		newAttributes = Object.assign(newAttributes, {
+			conversations: {
+				conversation_id: originalTaskSid,
+			},
+		});
+	}
 
-    // setup new task's attributes such that its linked to the
-    // original task in Twilio WFO
-    if (!newAttributes.hasOwnProperty('conversations')) {
-      newAttributes = Object.assign(newAttributes, {
-          conversations: {
-              conversation_id: originalTaskSid
-          }
-      })
-    }
+	// update task attributes to ignore the agent who transferred the task
+	// its possible that the agent who transferred the task in the queue
+	// the task is being transferred to - but we don't want them to
+	// receive a task they just transferred. It's also possible the agent
+	// is simply transferring to the same queue the task is already in
+	// once again, we don't want the transferring agent to receive the task
+	newAttributes.ignoreAgent = workerName;
 
-    // update task attributes to ignore the agent who transferred the task
-    // its possible that the agent who transferred the task in the queue
-    // the task is being transferred to - but we don't want them to
-    // receive a task they just transferred. It's also possible the agent
-    // is simply transferring to the same queue the task is already in
-    // once again, we don't want the transferring agent to receive the task
-    newAttributes.ignoreAgent = workerName;
+	// update task attributes to put the required targetSid on the task
+	// this could be either a workerSid or a queueSid
+	newAttributes.targetSid = targetSid;
 
-    // update task attributes to put the required queue sid on the task
-    // your workflow would need to be updated to honor these values
-    // and ensure the task makes it to the correct agent/queue
-    newAttributes.requiredQueue = destinationQueueSid;
+	// add an attribute that will tell our Workflow if we're transfering to a worker or queue
+	if (targetSid.startsWith('WK')) {
+		newAttributes.transferTargetType = 'worker';
+	} else {
+		newAttributes.transferTargetType = 'queue';
+	}
 
-    // create New task
-    let newTask = await client.taskrouter.workspaces(context.TWILIO_WORKSPACE_SID).tasks.create({
-        workflowSid: context.TWILIO_WORKFLOW_SID,
-        taskChannel: originalTask.taskChannelUniqueName,
-        attributes: JSON.stringify(newAttributes)
-    });
+	// create New task
+	let newTask = await client.taskrouter.workspaces(context.TWILIO_WORKSPACE_SID).tasks.create({
+		workflowSid: context.TWILIO_WORKFLOW_SID,
+		taskChannel: originalTask.taskChannelUniqueName,
+		attributes: JSON.stringify(newAttributes),
+	});
 
-    // complete old task
-    let completedTask = await client.taskrouter.workspaces(context.TWILIO_WORKSPACE_SID).tasks(originalTaskSid).update({
-        assignmentStatus: 'completed',
-        reason: 'task transferred'
-    });
+	if (mode == 'COLD') {
+		// Set the channelSid and ProxySessionSID to a dummy value. This keeps the session alive
+		let updatedAttributes = {
+			...JSON.parse(originalTask.attributes),
+			channelSid: 'CH00000000000000000000000000000000',
+			proxySessionSID: 'KC00000000000000000000000000000000',
+		};
 
-    response.setBody({
-        taskSid: newTask.sid
-    });
+		await client.taskrouter
+			.workspaces(context.TWILIO_WORKSPACE_SID)
+			.tasks(originalTaskSid)
+			.update({
+				attributes: JSON.stringify(updatedAttributes),
+			});
 
-    callback(null, response);
+		// Close the original Task
+		await client.taskrouter
+			.workspaces(context.TWILIO_WORKSPACE_SID)
+			.tasks(originalTaskSid)
+			.update({ assignmentStatus: 'completed', reason: 'task transferred' });
+	}
+
+	response.setBody({
+		taskSid: newTask.sid,
+	});
+
+	callback(null, response);
 });
