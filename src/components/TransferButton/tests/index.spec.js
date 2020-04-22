f9d7fa95d42929e801f2934e926ebcc80a963392
John Fischelli <john.fischelli@gmail.com>
2020-04-22 09:47:45 -0500

    adding tests on a new branch

---
+++ src/components/TransferButton/tests/index.spec.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import TransferButton from '..';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Actions } from '@twilio/flex-ui';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock('@twilio/flex-ui');
+jest.mock('flex-plugin');
+
+describe('Chat Transfer Button', () => {
+	let subject;
+
+	beforeEach(() => {
+		subject = Enzyme.mount(<TransferButton />);
+	});
+
+	it('calls show directory when clicked', () => {
+		subject.simulate('click');
+		expect(Actions.invokeAction).toHaveBeenCalledWith('ShowDirectory');
+	});
+});
